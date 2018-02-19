23551130a9f3c53e1f44b339f87e74e2c85aa84c
Homish <james@homish.com>
2018-02-19 15:40:26 -0800

    Inital commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=jstewardluisbot-Web-Deploy.pubxml -p:Password=KynEt0nkMisdA0dW3Ak3fMweoPasRB2saarCemPmJBSn3lN5d7W8vSLt3Kb5
+
